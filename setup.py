14c2f4caf63653dc456b9e087b714acc4aa0f759
Mario Kicherer <dev@kicherer.org>
2023-12-25 19:12:06 +0100

    version 2023.12.25 with pyproject.toml

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pysdbus',
-    version='20190924',
+    version='2023.12.25',
     description='pysdbus is a Python wrapper library for DBus inter-process communication',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/anyc/pysdbus/',
     download_url='https://github.com/anyc/pysdbus//archive/master.zip',
-    keywords=['dbus', 'sdbus', 'IPC'],
+    keywords=['dbus', 'sdbus', 'IPC', 'ctypes'],
     author='Mario Kicherer',
     author_email='dev@kicherer.org',
     license='LGPL-2.1',
